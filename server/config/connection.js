e24ed34962b0f9e0b7d64430ff1a1db9e2763847
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-07-27 18:07:31 -0700

    added typeDefs and resolvers

---
+++ server/config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// use Mongoose for all of its MongoDB data handling
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
