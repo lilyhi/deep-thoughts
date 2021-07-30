4c1790a178457c062195c0b0467a60e43ee04530
lilyhi <79773301+lilyhi@users.noreply.github.com>
2021-07-29 21:55:02 -0700

    added client and one serveronly

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+// line 9 and after was not in the code snapshot they had whats below only
+// const client = new ApolloClient({
+//   uri: '/graphql'
+// });
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// line 4 from snapshot
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -24,6 +23,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // listen for connection be made with db.open()
 db.once('open', () => {
